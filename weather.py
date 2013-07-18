7dc085603f90ff26ae751e4e40271b344e56bce7
testamajig <chuckwildebeest@gmail.com>
2013-07-18 17:02:56 +0000

    updated weather.py

--- weather.py
+++ weather.py
@@ -13,19 +13,12 @@ import math
 CARBON_SERVER = '127.0.0.1'
 CARBON_PORT = 2003
 
-delay = 600
-if len(sys.argv) > 1:
-  delay = int( sys.argv[1] )
-
 def get_weather_by_city(city):
     params = { 'q': city }
     url = 'http://api.openweathermap.org/data/2.1/find/name?' + urllib.urlencode(params)
     response = json.load(urllib2.urlopen(url))
 
-    #print json.dumps(response, indent=4)
-
     lines = []
-
     response = response['list'][0]
     celsius = round(float(response['main']['temp']) - 273.15, 2)
     fahrenheit = round(9.0 / 5.0 * celsius + 32, 2)
@@ -48,8 +41,6 @@ except:
   print "Couldn't connect to %(server)s on port %(port)d, is carbon-agent.py running?" % { 'server':CARBON_SERVER, 'port':CARBON_PORT }
   sys.exit(1)
 
-
-#while True:
 now = int( time.time() )
 lines = []
 
@@ -66,4 +57,3 @@ message = '\n'.join(lines) + '\n' #all lines must end in a newline
 #print message
 #print
 sock.sendall(message)
-#time.sleep(delay)
